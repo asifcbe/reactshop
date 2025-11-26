86b1173bc9ad568467ae36d078de0c72a5b08bb2
Asif Abdul Jaleel [PC-BLR-DEV] <asif.abdul@pointcross.net>
2025-11-26 09:53:10 +0530

    added database

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Home from "./routes/home/Home";
 import {Routes,Route} from 'react-router-dom'
 import Navigation from "./routes/navigation/Navigation";
+import SignIn from "./routes/sign-in/SignIn";
 function Shop() {
   return <div>Shop Page</div>;
 }
@@ -12,7 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigation />} >
         <Route index element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="signin" element={<SignIn />} />
         </Route>
       </Routes>
     </>
---
+++ src/utils/firebaseutils.js
@@ -0,0 +1,52 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInWithRedirect,
+} from "firebase/auth";
+import { getDoc, doc, setDoc, getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAd9nWsztF7ZrFo7SifjPjCjfMX-sfi5CY",
+  authDomain: "resig-ba1c0.firebaseapp.com",
+  projectId: "resig-ba1c0",
+  storageBucket: "resig-ba1c0.firebasestorage.app",
+  messagingSenderId: "188882569313",
+  appId: "1:188882569313:web:b6e3b4cc908acc3b3b4220",
+  measurementId: "G-328ZQT1QMP",
+};
+
+// Initialize Firebase
+const firebaseapp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+  if(!userSnapshot.exists()){
+    const {displayName,email} = userAuth;
+    const createdAt = new Date();
+  try{
+    await setDoc(userDocRef,{
+      displayName,
+      email,
+      createdAt
+    });
+  } catch (error) {
+    console.log("error creating the user", error.message);
+  }
+  }
+  return userDocRef;
+}
